b6c3189f5c38877d7925228a6f3e42cdc1c9229e
grueningp <pg@prcmail.de>
2021-11-16 12:21:13 +0100

    create data directories

--- exe_unpack_supplemental.py
+++ exe_unpack_supplemental.py
@@ -8,8 +8,10 @@ Once the code ran successfully, you can remove the folder 'saved_models'.
 
 """
 import shutil
-from DLBio.helpers import load_json, search_in_all_subfolders
-from os.path import dirname, splitext, join, isfile, isdir
+import warnings
+from os.path import dirname, isdir, isfile, join, splitext
+
+from DLBio.helpers import check_mkdir, load_json, search_in_all_subfolders
 
 ARCHIVE_PATH = 'jov_data.zip'
 EXTRACT_DIR = 'saved_models'
@@ -33,7 +35,9 @@ def _move_files_to_destination(file_paths, base_folder):
         ext = splitext(dst)[-1]
         src = join(base_folder, str(ctr).zfill(ZFILL) + ext)
         assert isfile(src), f'No file found at: {src}'
-        assert isdir(dirname(dst)), f'Directory does not exist: {dst}'
+        if not isdir(dirname(dst)):
+            check_mkdir(dirname(dst))
+            warnings.warn(f'Created directory: {dirname(dst)}')
 
         print(f'Moving {src}->{dst}')
         shutil.move(src, dst)
